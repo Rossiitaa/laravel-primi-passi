db3ca79d9a115235229950f808cba149e45dc08e
Rossiitaa <rosario.scuotto@outlook.it>
2022-09-06 15:11:45 +0200

    link created to go to the second page

--- resources/views/second-page.blade.php
+++ resources/views/second-page.blade.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Secondo Page</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
-    <a href="collegamentopagine">CLICK HERE!!</a>
+    <h1>THIS IS THE SECOND PAGE CREATED WITH LARAVEL</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/collegamentopagine', function () {
+    return view('second-page');
+});
